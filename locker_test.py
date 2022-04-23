d21914a01618cf76e761b06e77586ba5ec704a06
Liz <adhiamboliz3@gmail.com>
2022-04-23 15:21:14 +0300

    Added method to run before each test case

--- locker_test.py
+++ locker_test.py
@@ -1,2 +1,12 @@
 import unittest
 from user import User
+
+class TestClass(unittest.TestCase):
+    """
+    A Test class that defines test cases for the User class.
+    """
+    def setUp(self):
+        """
+        Method that runs before each individual test methods run.
+        """
+        self.new_user = User('Elizabeth','Adhiambo','liz','1234.')
