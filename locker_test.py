0d0f5bb3d322d34d9167fd9d68f01f5d5d9a5806
Liz <adhiamboliz3@gmail.com>
2022-04-23 15:31:40 +0300

    Ran save user test to confirm it fails

--- locker_test.py
+++ locker_test.py
@@ -18,6 +18,13 @@ class TestClass(unittest.TestCase):
             self.assertEqual(self.new_user.username,'liz')
             self.assertEqual(self.new_user.password,'1234.')
 
+    def test_save_user(self):
+        """
+        test case to test if a new user instance has been saved into the User list
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.userlist),1)
+
 
 if __name__ == "__main__":
     unittest.main()
