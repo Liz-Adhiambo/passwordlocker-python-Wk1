2af583f7091ac7ca3fcbaa88c3184857b18b9e5c
Liz <adhiamboliz3@gmail.com>
2022-04-23 15:26:39 +0300

    Ran first test to check if the user object has been initalized correctly

--- locker_test.py
+++ locker_test.py
@@ -10,3 +10,14 @@ class TestClass(unittest.TestCase):
         Method that runs before each individual test methods run.
         """
         self.new_user = User('Elizabeth','Adhiambo','liz','1234.')
+
+    def test_init(self):
+            """
+         test_init  Test case to chek if the object has been initialized correctly
+            """
+            self.assertEqual(self.new_user.username,'liz')
+            self.assertEqual(self.new_user.password,'1234.')
+
+
+if __name__ == "__main__":
+    unittest.main()
