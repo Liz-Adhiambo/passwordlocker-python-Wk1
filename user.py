9023e530b80d3da2b04bd148c085a8aa395610ea
Liz <adhiamboliz3@gmail.com>
2022-04-23 13:48:13 +0300

    Created the User class

---
+++ user.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+class User:
+    """
+    Create User class that generates new instances of a user.
+    """
+    userlist = []
+
+    def __init__(self,firstname,lastname, username, password):
+        """
+       _init_ method that defines the properties of the object self.
+        """
+        self.firstname=firstname
+        self.lastname=lastname
+        self.username = username
+        self.password = password
